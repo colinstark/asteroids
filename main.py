5564598f5f71d71482aed4de4a19f944c55a1adc
colinstark <starkinbox@gmail.com>
2024-12-07 17:27:09 +0100

    Updated constants and added containers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
 	pygame.init()
@@ -10,16 +10,31 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 	while True:
-
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
-
 		screen.fill("#000000")
-		pygame.display.flip
+
+		for obj in updatable:
+			obj.update(dt)
+
+		for obj in drawable:
+			obj.draw(screen)
+
+		pygame.display.flip()
+
+		delta = clock.tick(60)
+		dt = delta / 1000
 
 if __name__ == "__main__":
     main()
